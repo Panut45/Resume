29800d748c3df8ebcad35571a17a39f228a0b777
asteroid <92171358+Panut45@users.noreply.github.com>
2022-01-22 14:16:19 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/*==================== NEW SWIPER ====================*/
+var mySwiper = new Swiper (".my-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+});
